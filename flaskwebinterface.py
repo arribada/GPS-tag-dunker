f7d547c3b0c1324595651ecb6237d0a4684d9124
Tom Southworth <tomsouthworth@hotmail.co.uk>
2020-11-09 12:05:46 +0000

    Building the web interface for the GPS Tag Dunker.  Introducing JSON parsing for the Settings.json file, and a Schedule class, so that we can later on have an array of Schedules.

--- flaskwebinterface.py
+++ flaskwebinterface.py
@@ -8,8 +8,21 @@ import datetime
 from time import sleep
 import RPi.GPIO as GPIO
 import pause
+import json;
+import os;
 
 # VARIABLE SETUP
+# GPS TAG DUNKER SETTINGS VARIABLES
+# The file path of the Settings file.
+settingsFilePath = os.getcwd() + "/settings.json";
+
+# Allows for reading whether the GPS Tag Dunker is currently dunking.
+isDunking = False;
+
+# Create an array for all the different schduled lifts to go into.
+scheduledLifts = []
+
+# WINCH CONTROL VARIABLES
 # Clean up any errors from last execution (ensures winch not reeling).
 GPIO.setwarnings(False)
 GPIO.cleanup()
@@ -26,8 +39,41 @@ GPIO.setup(winchOutPin, GPIO.OUT)
 # Define the delay time between winch movements (for safety).
 delay = 1
 
-# Create an array for all the different schduled lifts to go into.
-scheduledLifts = []
+
+# CLASSES CODE
+# Define the Schedule Class
+class Schedule:
+    """The class which holds Schedule data"""
+
+    def __init__(self, name, colour, strength):
+        """Initialise the Cheese attributes"""
+        self.name      = name
+        self.colour    = colour
+        self.strength  = strength
+
+    def get_cheese_info(self):
+        """Returns this Cheese's Info"""
+        return (f"{self.name.title()} has a {self.colour} colour and an {self.strength} taste.")
+
+    def update_cheese_info(self, name, colour, strength):
+        """Updates this Cheese's Strength if possible"""
+        self.cheese_info['name']      = cheese_info['name']
+        self.cheese_info['colour']    = cheese_info['colour']
+        self.cheese_info['strength']  = cheese_info['strength']
+
+
+# SETTINGS CODE
+# Loads in the JSON settings data
+def LoadSettings():
+
+	with open(settingsFilePath) as file_object:
+		settings = json.load(file_object);
+
+	isDunking = settings["State"][0]["isDunked"];
+	#isDunking 
+
+
+LoadSettings();
 
 
 # WINCH CONTROL CODE
@@ -62,17 +108,17 @@ def ReadSchedule():
 # Create basic website.
 app = Flask(__name__)
 
-# This is the Main Page of the GPS Tag Dunker.
+# This is the main/index page of the GPS Tag Dunker web interface.
 @app.route('/')
 def index():
 	now = datetime.datetime.now()
 	dateTime = now.strftime("%Y-%m-%d %H:%M")
 	templateData = {
 		'title' : 'GPS Tag Dunker',
-		'button' : dateTime
+		'time' : dateTime
 		}
 	return render_template('index.html', **templateData)
 
 # Runs the Flask server and website.
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, port=80, host='0.0.0.0')
